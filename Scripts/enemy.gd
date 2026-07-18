c55a581b5ba316cc6b93ca808663651c96c72755
Jono <160443350+JonoBeach@users.noreply.github.com>
2026-07-18 14:48:15 +1200

    Fixed minor bugs
    - Fixed issue where game crashes if Enemy_overlap.monitoring is false

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -36,9 +36,10 @@ func _physics_process(_delta):
 		velocity = Vector2.ZERO
 		pushdirection = ""
 		set_position(pushend)
-	if $Enemy_overlap.get_overlapping_areas().size()>0:
-		$Enemy_overlap.get_overlapping_areas()[0].get_parent().killed(Vector2(0,0))
-		$Enemy_overlap/CollisionShape2D.disabled = true
+	if $Enemy_overlap.monitoring:
+		if $Enemy_overlap.get_overlapping_areas().size()>0:
+			$Enemy_overlap.get_overlapping_areas()[0].get_parent().killed(Vector2(0,0))
+			$Enemy_overlap/CollisionShape2D.disabled = true
 	move_and_slide()
 
 func attack_indicate():
